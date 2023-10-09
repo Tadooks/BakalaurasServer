fa19441711449920fcf4f285735ea7df0125ec88
Tadas <75209259+Tadooks@users.noreply.github.com>
2023-10-09 21:33:34 +0300

    Update firebaseDB.js

--- Backend/db/firebaseDB.js
+++ Backend/db/firebaseDB.js
@@ -7,14 +7,7 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjLcKDKBz0RsDqWxXbFxWw_1cZazlHR_o",
-    authDomain: "bakalauras-8fdcd.firebaseapp.com",
-    databaseURL: "https://bakalauras-8fdcd-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bakalauras-8fdcd",
-    storageBucket: "bakalauras-8fdcd.appspot.com",
-    messagingSenderId: "709385383917",
-    appId: "1:709385383917:web:db15e427bf728a5abf41e4",
-    measurementId: "G-RB8XZ1X5LJ"
+
 
 };
 
